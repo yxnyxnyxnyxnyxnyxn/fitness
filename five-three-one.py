8973a5e991d038e7a062b3aff192b3bbec9bb396
yxnyxnyxnyxnyxnyxn <yxn1116@gmail.com>
2022-12-12 21:26:15 -0500

    add stable workout

--- five-three-one.py
+++ five-three-one.py
@@ -7,15 +7,45 @@ import json
 import requests
 
 
+
+"""
+Highlevel Junk:
+Each day 
+1. one main workout 
+2. stable work
+3. random workouts
+
+stable workout 5 sets -> json 
+
+
+
+Goal: 
+Week1 
+
+Leg  Squat 
+Push Bench
+Pull LightDeadLift 
+Leg  Squat 
+Push OHP
+Pull DeadLift
+"""
+
+
+
 #KEYS
 API_NINJAS_KEY = os.getenv('API_NINJAS_KEY')    
-print(API_NINJAS_KEY)
+
 #CONSTANTS
 DAYS = ["Leg", "Push", "Pull"] * 2 
-MAIN_WORKOUT = ["Squat", "Bench", "Light Deadlift", "Squat", "OHP", "Light Deadlift"]
+MAIN_WORKOUT = ["Squat", "Bench", "Light Deadlift", "Squat", "OHP", "Deadlift"]
 PERCENT = [75,85,95]
 LIGHT_PERCENT = 65
 NINJAS_GET_URL = "https://api.api-ninjas.com/v1/exercises?" 
+STABLES = {
+	"Leg" : ["Glute Bridge", "Lunge", "Leg Press"],
+	"Push": ["Bench","Incline Bench", "Dumbbell Shoulder Press", "Lateral Raise"],
+	"Pull": ["Single Hand Row", "Pull Up"]
+}
 
 def get_call(api_url):
 	response = requests.get(api_url, headers={'X-Api-Key': API_NINJAS_KEY})
@@ -24,6 +54,12 @@ def get_call(api_url):
 def get_execises_by_muscle(muscle):
 	by_muscle_url = f"{NINJAS_GET_URL}muscle={muscle}"
 	return get_call(by_muscle_url)
+
+
+def stable_workout(day,main):
+	if main == "Bench": 
+		return STABLES[day][1:]
+	return STABLES[day]
 
 def calcualte_weight():
 	pr = open('pr.json')
@@ -38,16 +74,18 @@ def calcualte_weight():
 				max_w = p*pr[main]/100.00
 			else:
 				max_w = LIGHT_PERCENT*pr[main.split(" ")[1]]/100.00
-			m[week][main] = math.ceil(max_w)
-	
+			m[week][day] = [f"{main}: {math.ceil(max_w)}"]
+			m[week][day] += stable_workout(day,main)
+	return m 	
 
 
 def main():
-	#calcualte_weight()
-	print(get_execises_by_muscle("glutes").json())
+	print(calcualte_weight())
+	#print(get_execises_by_muscle("glutes").json())
 	return 0 
 
 
 
 if __name__ == "__main__":
 	main()
+	
